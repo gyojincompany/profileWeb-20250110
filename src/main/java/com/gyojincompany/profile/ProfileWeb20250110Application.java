d7e07249fc3fadfe0a401f1dcd58a8dafc749bca
luckyred <luckyred@gmail.com>
2025-01-10 10:50:18 +0900

    oralce, mybatis, jsp 환경설정 추가

---
+++ src/main/java/com/gyojincompany/profile/ProfileWeb20250110Application.java
@@ -0,0 +1,13 @@
+package com.gyojincompany.profile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfileWeb20250110Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfileWeb20250110Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/gyojincompany/profile/ProfileWeb20250110ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gyojincompany.profile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfileWeb20250110ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
