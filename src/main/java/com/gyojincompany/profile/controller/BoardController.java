698c6b3a7d70b4289f45368a2822b2891683720c
gyojincompany <luckyred@gmail.com>
2025-01-17 11:42:44 +0900

    페이지별 글 불러오기 구현 dao, sql 수정 추가

--- src/main/java/com/gyojincompany/profile/controller/BoardController.java
+++ src/main/java/com/gyojincompany/profile/controller/BoardController.java
@@ -69,13 +69,16 @@ public class BoardController {
 		
 		String pageNum = request.getParameter("pageNum");//사용자가 클릭한 게시판 페이지 번호(ex:3페이지)
 		
-		criteria.setPageNum(Integer.parseInt(pageNum));
+		if(pageNum != null) {//그냥 list 요청으로 오면 pageNum->null
+			criteria.setPageNum(Integer.parseInt(pageNum));
+		}
 //		사용자가 클릭한 페이지 번호를 criteria 객체 내의 멤버변수인 pageNum 값으로 설정
 		int total = bDao.totalBoardCountDao();//게시판 내 모든 글의 갯수
 		
 		PageDto pageDto = new PageDto(total, criteria);
 		
-		ArrayList<BoardDto> bDtos = bDao.listDao();//모든 글 가져오기
+		ArrayList<BoardDto> bDtos = bDao.listDao(criteria.getAmount(), criteria.getPageNum());
+		//인수로 한페이지에 보여질 글의 갯수와 사용자가 클릭한 페이지의 번호를 입력해서 호출
 		
 		model.addAttribute("bDtos", bDtos);
 		
--- src/main/java/com/gyojincompany/profile/dao/BoardDao.java
+++ src/main/java/com/gyojincompany/profile/dao/BoardDao.java
@@ -7,7 +7,8 @@ import com.gyojincompany.profile.dto.BoardDto;
 public interface BoardDao {
 	
 	public void writeDao(String bid, String bname, String btitle, String bcontent);//글쓰기
-	public ArrayList<BoardDto> listDao();//모든 글 가져오기
+	public ArrayList<BoardDto> listDao(int amount, int pageNum);//모든 글 가져오기
+	//amount->한 페이지에 보여질 글의 갯수, pageNum->사용자가 클릭한 페이지 번호
 	public BoardDto contentViewDao(String bnum);//글 번호로 해당 번호글의 모든 정보 가져오기
 	public void contentModifyDao(String bnum, String btitle, String bcontent);//글 수정하기
 	public int contentDeleteDao(String bnum);//글 삭제하기 
